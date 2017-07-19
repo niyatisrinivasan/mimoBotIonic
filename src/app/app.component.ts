b6e8e66bf74680fe716ff1d44451c159dbbc4b99
niyatisrinivasan <niyati.srinivasan@gmail.com>
2017-07-19 18:38:25 +0800

    Testing Noti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,53 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { Push, PushObject, PushOptions } from '@ionic-native/push'
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public push: Push, public alertCtlr: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.pushsetup();
     });
+  }
+  pushsetup() {
+    const options: PushOptions = {
+      android: {
+        senderID: '467862247498'
+      },
+      ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+
+      },
+      windows: {}
+    };
+    const PushObject: PushObject = this.push.init(options);
+
+    PushObject.on('notification').subscribe((notification: any) => {
+        if(notification.additionalData.foreground){
+            let youralert = this.alertCtlr.create({
+              title: 'New Push Notification',
+              message: notification.message
+            });
+            youralert.present();
+        }
+    });
+
+    PushObject.on('registration').subscribe((registration: any) => alert('Device registered'+ registration));
+
+    PushObject.on('error').subscribe(error => alert('Error with push plugin'+ error));
+
   }
 }
 
